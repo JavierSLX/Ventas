a38c01277744b21577642b1c7c75759cf8a5cb90
JavierSLX <j.serranolule@ugto.mx>
2017-07-08 09:16:46 -0500

    Merge hasta el 08/07/2017

--- VentaPrinc/stdafx.h
+++ VentaPrinc/stdafx.h
@@ -44,5 +44,4 @@ using namespace std;
 #define DSN L"dsn_MyDatabase"
 #define USERNAME L"root"
 #define PASSWORD L"123"
-<<<<<<< HEAD
 #define CONNECTION_STRING L"Driver={MySQL ODBC 5.3 UNICODE Driver};Server=localhost;Database=ventas;Port=3300;User=root;Password=root;Option=1;"
